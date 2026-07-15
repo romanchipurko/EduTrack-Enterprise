4f68dd7663f2dc7b20299a2ab7aab15f465c3b58
Paprika991 <romachipurko@gmail.com>
2026-07-15 18:01:15 +0300

    [6] fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
   root "home#index"
 
-  scope "/:locale", locale: /en|ru/ do
+  scope "(:locale)", locale: /en|ru/ do
+    ActiveAdmin.routes(self)
     get "/", to: "home#index"
     devise_for :users
     get "profile", to: "users#show"
